3f3bd422b5223225a4c40a3964a57615e304a020
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-28 02:01:07 +0900

    [#5] set: :file_folder: 에러 및 성공 상수 관리

---
+++ server/src/constants/success.js
@@ -0,0 +1,31 @@
+module.exports = {
+	DEFAULT_READ: {
+		code: 200,
+		message: '성공적으로 조회 했습니다.',
+	},
+
+	DEFAULT_UPDATE: {
+		code: 200,
+		message: '성공적으로 수정 했습니다.',
+	},
+
+	DEFAULT_DELETE: {
+		code: 200,
+		message: '성공적으로 삭제 했습니다.',
+	},
+
+	DEFAULT_CREATE: {
+		code: 200,
+		message: '성공적으로 추가 했습니다.',
+	},
+
+	LOGIN: {
+		code: 200,
+		message: '성공적으로 로그인했습니다.',
+	},
+
+	SIGNUP: {
+		code: 200,
+		message: '성공적으로 회원가입 했습니다.',
+	},
+};
