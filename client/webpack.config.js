6a251889cb427c1b28ad524a6b5b1a7ac39ab08c
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-27 20:37:38 +0900

    [#4] set: :file_folder: babel, eslint, prettier, webpack 설정

---
+++ client/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: ['/node_modules'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+
+  optimization: { minimize: true },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.ts', '.js', '.json', '.scss'],
+  },
+  output: { path: path.join(__dirname, './dist'), filename: '[name].js' },
+  devtool: 'source-map',
+};
