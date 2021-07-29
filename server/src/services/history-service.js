2910787e5a3a5c2b08b311b55c38f3698e7aa750
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-29 23:21:20 +0900

    [#15] wip: :gear: query 문에서 보완해야할 점은 있으나, 데이터 조회 시버스 구현 완료

--- server/src/services/history-service.js
+++ server/src/services/history-service.js
@@ -3,23 +3,96 @@ const success = require('../constants/success');
 const error = require('../constants/error');
 const CustomError = require('../errors/custom-error');
 
+const Op = db.Sequelize.Op;
 
-const getHistory = async (req, res, next) => {
+const getAllHistoryByFilter = async (req, res, next) => {
     try {
-        // const { id, password } = req.body;
-        // const [user, isCreated] = await db.User.findOrCreate({
-        //     where: { id },
-        //     defaults: { password: createHash(password) }
-        // });
-        // if (!isCreated) {
-        //     throw new CustomError(error.EXIST_USER_ID_ERROR);
-        // }
-        // const { code, message } = success.DEFAULT_CREATE;
-        // res.status(code).json({ message, user });
+
+        let { 
+            startDate,      // '2021-05-20'
+            endDate,        // '2021-08-20'
+            CategoryPk,     // '2'
+            content,        // '씨유한양대'
+            PayTypePk,      // '1'
+            minimumPrice,   // '10000'
+            maximumPrice    // '130000'
+        } = req.query;
+
+        const where = {};
+
+        if (startDate && endDate) {
+            where.time = {
+                [Op.between]: [new Date(startDate), new Date(endDate)], // 추후 다음날 오전 12시로 처리해야한다. 
+            };
+        } else if (startDate) {
+            where.value = {
+                [Op.gte]: new Date(startDate),
+            }
+        } else if (endDate) {
+            where.value = {
+                [Op.lte]: new Date(endDate),
+            }
+        }
+
+        if (CategoryPk) {
+            where.CategoryPk = CategoryPk;
+        }
+
+        if (PayTypePk) {
+            where.PayTypePk = PayTypePk;
+        }
+
+        if (content) {
+            where.content = {
+                [Op.like]: `%${content}%`
+            };
+        }
+
+        if (minimumPrice && maximumPrice) {
+            where.value = {
+                [Op.between]: [minimumPrice, maximumPrice],
+            };
+        } else if (minimumPrice) {
+            where.value = {
+                [Op.gte]: minimumPrice,
+            }
+        } else if (maximumPrice) {
+            where.value = {
+                [Op.lte]: maximumPrice,
+            }
+        }
+
+        const historys = await db.History.findAll({ where });
+
+        if (!historys) {
+            throw new CustomError(error.NO_DATA);
+        }
+
+        const { code, message } = success.DEFAULT_READ;
+
+        res.status(code).json({ message, historys, user: req.user });
     } catch (e) {
         next(e); 
     }
 }
 
 
-module.exports = {getHistory};
+const getAllHistory = async (req, res, next) => {
+    try {
+
+        const historys = await db.History.findAll();
+
+        if (!historys) {
+            throw new CustomError(error.NO_DATA);
+        }
+
+        const { code, message } = success.DEFAULT_READ;
+
+        res.status(code).json({ message, historys, user: req.user });
+    } catch (e) {
+        next(e); 
+    }
+}
+
+
+module.exports = {getAllHistoryByFilter, getAllHistory};
