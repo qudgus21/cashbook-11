f820b68d668d624933f22069b3a7f9f7dc4bc2cc
0woodev <tc_yeongu.n@woowafriends.com>
2021-07-29 17:06:19 +0900

    [#6] feat: :sparkles: 모달에서 로그인 회원가입 구현

---
+++ client/src/components/base/user-modal/index.ts
@@ -0,0 +1,38 @@
+
+import './index.scss';
+import Component from "../../../core/component";
+import api from "../../../utils/api";
+import { $ } from '../../../utils/select';
+import { addClassSelector } from '../../../utils/selectHandler';
+import ModalContent from './modal-content';
+
+export default class Modal extends Component {
+
+    setup () {
+        this.state = {};
+    }
+    
+    template (): string { 
+        return `
+            <div class="modal hidden">
+                <div class="modal-background"></div>
+                <div class="modal-content"></div>
+            </div>
+        `;
+    }
+
+
+    mounted () {
+        const $modalContent = $('.modal .modal-content').get();
+
+        new ModalContent($modalContent);
+    }
+
+    setEvent() {
+        this.addEvent('click','.modal .modal-background', (e) => {
+            e.preventDefault();
+            addClassSelector($('.modal').get(), 'hidden');
+        });
+    }
+}
+
