0a42d195e611b95db2963c37a3009f11c13d348a
황병현 <>
2021-07-30 03:48:34 +0900

    [#18] wip: :gear: history 전부 가져오기 api

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const userRouter = require('./user-router');
 const homeRouter = require('./home-router');
+const calendarRouter = require('./calendar-router');
 
 const router = express.Router();
 
 router.use('/user', userRouter);
 router.use('/home', homeRouter);
+router.use('/calendar', calendarRouter);
 
 module.exports = router;
