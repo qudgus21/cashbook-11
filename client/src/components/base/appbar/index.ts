a25d9eac24085a6b22333f2c466a3e5918415ea6
황병현 <>
2021-07-29 15:32:56 +0900

    [#10] wip: :gear: appbar 라우팅

--- client/src/components/base/appbar/index.ts
+++ client/src/components/base/appbar/index.ts
@@ -1,62 +1,85 @@
 import Component from "../../../core/component";
 import { navigateTo } from "../../../core/router";
 import { dateStore } from "../../../models";
+import { $ } from "../../../utils/select"
 import './index.scss';
 
 export default class Appbar extends Component {
     
+
     template (): any { 
         return ` 
             <div class="container-appbar">
-                <div>우아한 가계부</div>
-                <div>
-                    <button 
-                        class="button button-prev"
-                    >&lt;</button>
-                    ${dateStore.state.year}-${dateStore.state.month}
-                    <button 
-                        class="button button-next"
-                    >&gt;</button>
+                <h1 class="title">우아한 가계부</h1>
+                <div class="date-controll">
+                    <div class="button-prev">
+                        <img src="../../../src/assets/chevron-left.svg"/>
+                    </div>
+                    <div class="date">
+                        <div>${dateStore.state.month}월</div>
+                        <div>${dateStore.state.year}</div>                   
+                    </div>
+                    <div class="button-next">
+                        <img src="../../../src/assets/chevron-right.svg"/>
+                    </div>
                 </div>
-                <div>
-                    <button 
-                        class="button button-home"
-                        data-index-number="0"
-                    >홈</button>
-                    
-                    <button 
-                        class="button button-calendar"
-                        data-index-number="1"
-                    >달력</button>
-                    <button 
-                        class="button button-statistics"
-                        data-index-number="2"
-                    >통계</button>
+                <div class="page-controll">
+                    <div class="button-home">
+                        <img src="../../../src/assets/file-text.svg"/>
+                    </div>
+                    <div class="button-calendar">
+                        <img src="../../../src/assets/chart.svg"/>
+                    </div>
+                    <div class="button-statistics">
+                        <img src="../../../src/assets/calendar.svg"/>
+                    </div>
                 </div>
             </div>
         `
     }
 
-    mounted () {
-        const wrapperAppbar = document.querySelector('.wrapper-appbar');
+
+    currentPageImg(history?: any) {
+        const $imgs = $('.container-appbar .page-controll img').getAll();
+        $imgs.forEach(img => { img.classList.remove('active') });
+        const currentPage = location.pathname.split('/').pop();
+        switch (history || currentPage) {
+            case 'home':
+                $imgs[0].classList.add('active');
+                break;
+            case 'calendar':
+                $imgs[1].classList.add('active');
+                break;
+            case 'statistics':
+                $imgs[2].classList.add('active');
+                break;
+        }
+    }
+
+
+    mounted() {
+        this.currentPageImg()
     }
 
     setEvent(){        
         this.addEvent('click', '.button-home', ({target})=>{
             navigateTo('/home');
+            this.currentPageImg()
         })
 
-        this.addEvent('click', '.button-calendar', ({target})=>{
+        this.addEvent('click', '.button-calendar', ({ target }) => {
             navigateTo('/calendar');
+            this.currentPageImg()
         })
 
         this.addEvent('click', '.button-statistics', ({target})=>{
             navigateTo('/statistics');
+            this.currentPageImg()
         })
 
         this.addEvent('click', '.button-prev', ({target})=>{
             dateStore.moveToPreviousMonth();
-            this.render(); // 바뀐 부분만 바꿔주는 것을 반영하면 좋을 것 같다.
+            this.render();
         })
 
         this.addEvent('click', '.button-next', ({target})=>{
