1d29452552bcc7dfe9363902214f1b10d17b665b
황병현 <>
2021-07-29 15:34:09 +0900

    [#10] set: :file_folder: home-router, service 추가

---
+++ server/src/services/history-service.js
@@ -0,0 +1,25 @@
+const db = require('../models');
+const success = require('../constants/success');
+const error = require('../constants/error');
+const CustomError = require('../errors/custom-error');
+
+
+const getHistory = async (req, res, next) => {
+    try {
+        // const { id, password } = req.body;
+        // const [user, isCreated] = await db.User.findOrCreate({
+        //     where: { id },
+        //     defaults: { password: createHash(password) }
+        // });
+        // if (!isCreated) {
+        //     throw new CustomError(error.EXIST_USER_ID_ERROR);
+        // }
+        // const { code, message } = success.DEFAULT_CREATE;
+        // res.status(code).json({ message, user });
+    } catch (e) {
+        next(e); 
+    }
+}
+
+
+module.exports = {getHistory};
