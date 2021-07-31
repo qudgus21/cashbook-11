0c647b9155e1e99d1025e9ff461b38a2ad860b6f
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-01 08:35:18 +0900

    [#15] wip: :gear: 일별 히스토리 제작

--- client/src/components/base/month-history/daily-history/index.ts
+++ client/src/components/base/month-history/daily-history/index.ts
@@ -1,1 +1,52 @@
+import './index.scss';
+import Component from "../../../../core/component";
+import api from "../../../../utils/api";
+import { $ } from '../../../../utils/select';
+import { addClassSelector, removeClassSelector } from '../../../../utils/selectHandler';
+import { checkLogin } from '../../../../utils/cookie';
+import UnitHistory from './unit-history';
+
+const isEmpty = (x) => (typeof x === 'undefined' || x === null || x === '');
+
+export default class DailyHistory extends Component {
+
+    setup () {
+        this.state = this.props;
+        console.log(this.state);
+        
+    }
+    
+    template (): string {
+        
+        return`
+            <div class="container-daily-history">
+                <div>${this.state.month}월 ${this.state.day}일 </div>
+                <div class="container-unit-history">${this.state.historys.map((x, idx)=> {
+                    return `
+                        <div 
+                            id="unit-history-${x.month}-${x.day}-${idx}"
+                            class="wrapper-unit-history"
+                        ></div>
+                    `;
+                }).join('\n')}</div>
+            </div>
+        `;
+    }
+
+    mounted () {
+        this.state.historys.forEach((history, idx) => {
+            console.log(`DailyHistory 의 UnitHistory를 추가해줍니다~~`);
+            console.log(`#unit-history-${history.month}-${history.day}-${idx} : `,`history: ${history}`);
+            new UnitHistory(
+                $(`#unit-history-${history.month}-${history.day}-${idx}`).get(),
+                { history }
+            );
+        });
+    }
+    
+    setEvent() {
+
+    }
+  
+}
 
