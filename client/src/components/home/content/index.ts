1176db09bad9cfd97844f75cc0773cd7b036e186
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-01 03:40:30 +0900

    [#15] jesus: :god: 신비로운 깃의 세계 하하핳핳핳

--- client/src/components/home/content/index.ts
+++ client/src/components/home/content/index.ts
@@ -2,8 +2,10 @@ import './index.scss';
 import Component from "../../../core/component";
 import api from "../../../utils/api";
 import { $ } from '../../../utils/select';
-import { removeClassSelector } from '../../../utils/selectHandler';
-import DailyHistory from '../../base/daily-history';
+import { addClassSelector, removeClassSelector } from '../../../utils/selectHandler';
+import DailyHistory from '../../base/month-history';
+import { isEmptyJWTToken } from '../../../utils/result-checker'; // TODO
+import Filter from '../filter';
 
 export default class Content extends Component {
 
@@ -13,13 +15,31 @@ export default class Content extends Component {
     
     template (): string { 
         return `
-            <div class="wrapper-content"></div>
+            <div class="container-filter">안녕하세요 누구신가요? </div>
+            <div class="wrapper-month-history"></div>
         `;
     }
 
     mounted () {
-        new DailyHistory($('.wrapper-content').get());
+        // TODO
+        if (isEmptyJWTToken()) { 
+            addClassSelector($('.container-filter').get(), 'hidden');
+            $('.wrapper-month-history').get().innerHTML = this.getLoginImgTemplate();
+        } else {
+            new Filter($('.container-filter').get());
+            new DailyHistory($('.wrapper-month-history').get());
+        }
     }
+
+    getLoginImgTemplate() {
+        return `
+        <div class="wrapper-img-login"> 
+            <img src="../../../src/assets/baedal.jpg" class="img-baedal" /> 
+            <div class= "footer"> 로그인을 먼저 하겠어! </div>
+        </div>
+        `;
+    }
+
 
     setEvent() {
         this.addEvent('click','.button-user', (e) => {
--- client/src/utils/result-checker.ts
+++
@@ -1,5 +0,0 @@
-import { getCookie } from "./cookie";
-
-const isEmpty = (str: any): boolean => (typeof str == 'undefined' || str == null || str== "");
-
-export const isEmptyJWTToken = (): boolean => isEmpty(getCookie('JWT'));
