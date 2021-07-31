cfdc745ea8e1d9b81f0762c17326fc829fb7fb8f
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-01 03:12:56 +0900

    [#15] set: :file_folder: filter 컴포넌트 제작

--- client/src/components/home/filter/index.ts
+++ client/src/components/home/filter/index.ts
@@ -1,1 +1,31 @@
+import './index.scss';
+import Component from "../../../core/component";
+import api from "../../../utils/api";
+import { $ } from '../../../utils/select';
+import { removeClassSelector } from '../../../utils/selectHandler';
+
+export default class Filter extends Component {
+
+    setup () {
+        this.state = {};
+        this.getData();
+    }
+
+    async getData() {
+       
+    }
+    
+    template (): string { 
+        return `
+            
+        `;
+    }
+
+
+    setEvent() {
+        this.addEvent('click','.button-search', async (e) => {
+            e.preventDefault();
+        });
+    }
+}
 
