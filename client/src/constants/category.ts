e24d6ffb17fedccc94dd479b7594f0603f9da47d
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-02 05:38:42 +0900

    [#15] wip: :gear: 카테고리 배경 색 및 텍스트 변수 관리

---
+++ client/src/constants/category.ts
@@ -0,0 +1,12 @@
+export const CATEGORY_TAG = [
+    { title: '생활', color: '#4A6CC3' },
+    { title: '식비', color: '#4C9FDE' },
+    { title: '교통', color: '#93D3CC' },
+    { title: '쇼핑/뷰티', color: '#4CB8B8' },
+    { title: '의료/건강', color: '#6DD5EB' },
+    { title: '문화/여가', color: '#D092E2' },
+    { title: '미분류', color: '#817DCE' },
+    { title: '월급', color: '#B9D58C' },
+    { title: '용돈', color: '#E2B766' },
+    { title: '기타수입', color: '#E2B766' },    
+]
