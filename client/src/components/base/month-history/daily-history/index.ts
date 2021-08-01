2e1440495633b5da379e91fce88c8b8129d8d3df
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-01 23:36:39 +0900

    [#15] wip: :gear: daily-history css 작업

--- client/src/components/base/month-history/daily-history/index.ts
+++ client/src/components/base/month-history/daily-history/index.ts
@@ -15,10 +15,15 @@ export default class DailyHistory extends Component {
     }
     
     template (): string {
-        
         return`
             <div class="container-daily-history">
-                <div>${this.state.month}월 ${this.state.day}일 </div>
+                <div class="daily-history-title-bar">
+                    <div>${this.state.month}월 ${this.state.day}일 ${this.state.dayOfWeek}</div>
+                    <div class="container-daily-history-total-price">
+                        <div class="text-consume">수입 ${this.state.income}원</div>
+                        <div class="text-consume">지출 ${this.state.consume}원</div>
+                    </div>
+                </div>
                 <div class="container-unit-history">${this.state.historys.map((x, idx)=> {
                     return `
                         <div 
