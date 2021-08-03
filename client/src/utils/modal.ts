47850ae4f34d227b054ed18aede328a665450dd1
황병현 <>
2021-08-03 20:54:57 +0900

    [#21] refactor: :hammer: 모달 창 opacity, disable 수정

--- client/src/utils/modal.ts
+++ client/src/utils/modal.ts
@@ -6,11 +6,26 @@ export const addModal = (): void => {
         $('.container-daybar').get(),
         $('.container-searchbar').get(),
         $('.appbar').get(),
+        $('.container-statistics').get(),
     ];
+
+    let zindex = [
+        $('.container-statistics').get(),
+    ]
    
     candidate.forEach((node) => { 
-        if(node) addClassSelector(node, 'opacity')
+        if (node) { 
+            addClassSelector(node, 'opacity')
+            node.style.zIndex = 0;
+            
+        }
     })
+
+    zindex.forEach((node) => { 
+        if (node) node.style.zIndex = 0
+    })
+
+    document.body.style.overflowY = 'hidden'
 
     removeClassSelector($('.modal').get(), 'hidden');
 }
@@ -22,11 +37,23 @@ export const removeModal = (): void => {
         $('.container-daybar').get(),
         $('.container-searchbar').get(),
         $('.appbar').get(),
+        $('.container-statistics').get(),
     ];
+
+    let zindex = [
+        $('.container-statistics').get(),
+    ]
+
 
     candidate.forEach((node) => { 
         if(node) removeClassSelector(node, 'opacity')
-
     })
+
+    zindex.forEach((node) => { 
+        if (node) node.style.zIndex = 1;
+    })
+
+    document.body.style.overflowY = 'scroll'
+
     addClassSelector($('.modal').get(), 'hidden');
 }
