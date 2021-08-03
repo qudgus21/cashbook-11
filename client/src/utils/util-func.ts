e7a316f6ec65c9f3c33b61d584cabdeb6d52ebe5
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-03 20:55:49 +0900

    [#15] wip: :gear: 유틸함수 제작

--- client/src/utils/util-func.ts
+++ client/src/utils/util-func.ts
@@ -1,1 +1,10 @@
 export const isEmpty = (x) => (typeof x === 'undefined' || x === null || x === '');
+
+export const getDateInfo = (time) => {
+    time = new Date(time);
+    const year = time.getFullYear();
+    const month = time.getMonth() + 1;
+    const date = time.getDate();
+    const day = time.getDay();
+    return { year, month, date, day };
+}
